6dde58f6bbd12463d1e350685ca6e04dc6ea1dbe
qin <991779596@qq.com>
2021-10-30 16:08:24 +0800

    second commit

---
+++ src/com/clq/git.java
@@ -0,0 +1,11 @@
+package com.clq;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("这是我的第一次git-idea代码");
+    }
+
+}
